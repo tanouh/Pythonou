dd83b6bf084f2ffb9c1415b8d17293e037e9b23a
garnierk <Kevin_Garnier@outlook.fr>
2023-10-10 16:02:32 +0200

    add print fct

--- runtime.ml
+++ runtime.ml
@@ -3,13 +3,28 @@ open Ast
 exception Error of string
 exception RuntimeError of string * Lexing.position
 
-let eval program_ast out =
-  let eval_expr (expr : Ast.expr) = expr in
-  let eval_stmt (stmt : Ast.stmt) = stmt in
-  let call fct args = match fct with "print" -> args in
 
+let eval program_ast out =
+  let rec eval_expr = function
+  | Ecall(fct, [arg1]) -> call fct [eval_expr arg1]; 0
+  | Const(Int s) -> int_of_string s
+  | _ -> raise (Error "NAN ! ")
+  and
+    eval_stmt (stmt_node,pos) = match stmt_node with
+  | Sval e -> let _ = eval_expr e in ()
+  | Sblock b ->  let _ = List.iter eval_stmt b in ()
+  | _ -> raise (RuntimeError ("Not implemented stmt !", pos))
+and
+   call fct args =
+    let main_fct = List.hd (program_ast.defs) in
+    assert (main_fct.name = "-#-main-#-");
+    match fct with
+    | "print" -> out (string_of_int (List.hd args))
+    | "-#-main-#-" -> let _ = eval_stmt main_fct.body in ();
+    | _ -> raise (Error "Function non implemented !")
+in
   (* FILL ME *)
-  let run (program : Ast.prog) =
+  let run (_program : Ast.prog) =
     (* FILL ME *)
     let _ = call "-#-main-#-" [] in
     ()
