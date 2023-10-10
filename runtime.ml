a07438b7c37979deb57d19d1e4cbf811c023db8f
garnierk <Kevin_Garnier@outlook.fr>
2023-10-10 14:55:32 +0200

    add files

--- runtime.ml
+++ runtime.ml
@@ -1,18 +1,17 @@
-open Ast ;;
- 
-exception  Error of string ;;
-exception  RuntimeError of string * Lexing.position ;;
-         
-let eval program_ast (out: string -> unit) = 
-  let eval_expr (expr:Ast.expr)=
-  in
-  let eval_stmt (stmt:Ast.stmt)=
-  in
-  let call _fct_name _args =
+open Ast
+
+exception Error of string
+exception RuntimeError of string * Lexing.position
+
+let eval program_ast out =
+  let eval_expr (expr : Ast.expr) = expr in
+  let eval_stmt (stmt : Ast.stmt) = stmt in
+  let call fct args = match fct with "print" -> args in
+
+  (* FILL ME *)
+  let run (program : Ast.prog) =
     (* FILL ME *)
+    let _ = call "-#-main-#-" [] in
     ()
   in
-  let run (program:Ast.prog) =
-  (* FILL ME *)
-  let _ = call "-#-main-#-" [] in ()
-  in run program_ast
+  run program_ast
