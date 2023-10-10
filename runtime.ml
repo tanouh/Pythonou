46809db00b55c0628513e27812868a0bd81af341
tmahandry <taniamahandry24@gmail.com>
2023-10-10 14:55:17 +0200

    gitignore

--- runtime.ml
+++ runtime.ml
@@ -2,11 +2,17 @@ open Ast ;;
  
 exception  Error of string ;;
 exception  RuntimeError of string * Lexing.position ;;
-
-let call _fct_name _args =
-  (* FILL ME *)
-  ()
-           
-let run (program:Ast.prog) =
+         
+let eval program_ast (out: string -> unit) = 
+  let eval_expr (expr:Ast.expr)=
+  in
+  let eval_stmt (stmt:Ast.stmt)=
+  in
+  let call _fct_name _args =
+    (* FILL ME *)
+    ()
+  in
+  let run (program:Ast.prog) =
   (* FILL ME *)
   let _ = call "-#-main-#-" [] in ()
+  in run program_ast
